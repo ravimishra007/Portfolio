53ca3789e6843df5187a1acdcd5df99dc1f229a3
ravi mishra <ravimishrafea.560722@gmail.com>
2024-04-19 21:17:27 +0530

    some changes in js

--- script.js
+++ script.js
@@ -123,28 +123,6 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
