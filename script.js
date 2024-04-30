13b2101a6b2ec5e62918d9a10a73ce12a6b11a6f
ravi mishra <ravimishrafea.560722@gmail.com>
2024-04-30 21:31:17 +0530

    some changes in project section

--- script.js
+++ script.js
@@ -256,11 +256,11 @@ container4.appendChild(img4);
 
 
 let imageArr4 = [
-//   "./images/Navbar-Figma.png",
+    "./images/navbar.png",
   "./images/Hemburger.png",
   "./images/Body-Figma1.png",
   "./images/Body-Figma2.png",
-//   "./images/Footer-Figma.png",
+  "./images/footer.png",
 ];
 
 let index4 = 0;
